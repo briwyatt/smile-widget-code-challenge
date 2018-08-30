73f3325025e35b58912f19e87e67ba9d33ddec49
Bri Wyatt <mbriwyatt@gmail.com>
2018-08-30 01:08:41 -0400

    add ProductPrice model

--- smilewidgets/products/admin.py
+++ smilewidgets/products/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Product, GiftCard
+from .models import Product, GiftCard, ProductPrice
 
 admin.site.register(Product)
 admin.site.register(GiftCard)
+admin.site.register(ProductPrice)
--- smilewidgets/products/models.py
+++ smilewidgets/products/models.py
@@ -4,14 +4,9 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=25, help_text='Customer facing name of product')
     code = models.CharField(max_length=10, help_text='Internal facing reference to product')
-    price = models.PositiveIntegerField(help_text='Price of product in cents')
 
     def __str__(self):
         return '{} - {}'.format(self.name, self.code)
-
-    @property
-    def formatted_price(self):
-        return '${0:.2f}'.format(self.price / 100)
 
 
 class GiftCard(models.Model):
@@ -26,3 +21,19 @@ class GiftCard(models.Model):
     @property
     def formatted_amount(self):
         return '${0:.2f}'.format(self.amount / 100)
+
+
+class ProductPrice(models.Model):
+    title = models.CharField(max_length=50, help_text='Description of this pricing schedule for product')
+    price = models.PositiveIntegerField(help_text='Price of product in cents')
+    date_start = models.DateField()
+    date_end = models.DateField(blank=True,null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} - {}: {}'.format(self.title, self.product.name, self.formatted_price)
+
+
+    @property
+    def formatted_price(self):
+        return '${0:.2f}'.format(self.price / 100)
