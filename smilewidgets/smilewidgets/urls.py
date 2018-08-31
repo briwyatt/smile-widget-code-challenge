3e15349580b01ad1184893709cb0d4eb46296620
Bri Wyatt <mbriwyatt@gmail.com>
2018-08-31 18:13:23 -0400

    built out logic in views.py at api/get-price url

--- smilewidgets/smilewidgets/urls.py
+++ smilewidgets/smilewidgets/urls.py
@@ -14,12 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 # """
 
-from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
-    url('api/', include('products.urls')),
+    path('admin/', admin.site.urls),
+    path('api/', include('products.urls')),
 ]
-
-
